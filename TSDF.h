73b1ecdce2ce8202f898fa37b15adc97a771af55
Kerem <kerem.yildirir@tum.de>
2020-12-28 18:38:51 +0100

    Initial setup

---
+++ TSDF.h
@@ -0,0 +1,14 @@
+//
+// Created by kerem on 28/12/2020.
+//
+
+#ifndef KINECTFUSION_TSDF_H
+#define KINECTFUSION_TSDF_H
+
+
+class TSDF {
+
+};
+
+
+#endif //KINECTFUSION_TSDF_H
---
+++ main.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <fstream>
+
+#include "Eigen.h"
+#include "VirtualSensor.h"
+#include "SimpleMesh.h"
+#include "ICPOptimizer.h"
+#include "PointCloud.h"
+
+#define SHOW_BUNNY_CORRESPONDENCES 0
+
+#define USE_POINT_TO_PLANE	1
+#define USE_LINEAR_ICP		1
+
+#define RUN_SHAPE_ICP		0
+#define RUN_SEQUENCE_ICP	1
+
+void preprocess(PointCloud &p){
+
+
+    //bilateral filter  here
+
+}
+
+
+
+int reconstructRoom() {
+	std::string filenameIn = std::string("../../rgbd_dataset_freiburg1_xyz/");
+	std::string filenameBaseOut = std::string("mesh_");
+
+	// Load video
+	std::cout << "Initialize virtual sensor..." << std::endl;
+	VirtualSensor sensor;
+	if (!sensor.init(filenameIn)) {
+		std::cout << "Failed to initialize the sensor!\nCheck file path!" << std::endl;
+		return -1;
+	}
+
+	// We store a first frame as a reference frame. All next frames are tracked relatively to the first frame.
+	sensor.processNextFrame();
+	PointCloud target{ sensor.getDepth(), sensor.getDepthIntrinsics(), sensor.getDepthExtrinsics(), sensor.getDepthImageWidth(), sensor.getDepthImageHeight() };
+	
+	// Setup the optimizer.
+	ICPOptimizer* optimizer = nullptr;
+	if (USE_LINEAR_ICP) {
+		optimizer = new LinearICPOptimizer();
+	}
+	else {
+		optimizer = new CeresICPOptimizer();
+	}
+
+	optimizer->setMatchingMaxDistance(0.1f);
+	if (USE_POINT_TO_PLANE) {
+		optimizer->usePointToPlaneConstraints(true);
+		optimizer->setNbOfIterations(10);
+	}
+	else {
+		optimizer->usePointToPlaneConstraints(false);
+		optimizer->setNbOfIterations(20);
+	}
+
+	// We store the estimated camera poses.
+	std::vector<Matrix4f> estimatedPoses;
+	Matrix4f currentCameraToWorld = Matrix4f::Identity();
+	estimatedPoses.push_back(currentCameraToWorld.inverse());
+
+	int i = 0;
+	const int iMax = 50;
+	while (sensor.processNextFrame() && i <= iMax) {
+		float* depthMap = sensor.getDepth();
+		Matrix3f depthIntrinsics = sensor.getDepthIntrinsics();
+		Matrix4f depthExtrinsics = sensor.getDepthExtrinsics();
+
+		// Estimate the current camera pose from source to target mesh with ICP optimization.
+		// We downsample the source image to speed up the correspondence matching.
+		PointCloud source{ sensor.getDepth(), sensor.getDepthIntrinsics(), sensor.getDepthExtrinsics(), sensor.getDepthImageWidth(), sensor.getDepthImageHeight(), 8 };
+		currentCameraToWorld = optimizer->estimatePose(source, target, currentCameraToWorld);
+		
+		// Invert the transformation matrix to get the current camera pose.
+		Matrix4f currentCameraPose = currentCameraToWorld.inverse();
+		std::cout << "Current camera pose: " << std::endl << currentCameraPose << std::endl;
+		estimatedPoses.push_back(currentCameraPose);
+
+		if (i % 5 == 0) {
+			// We write out the mesh to file for debugging.
+			SimpleMesh currentDepthMesh{ sensor, currentCameraPose, 0.1f };
+			SimpleMesh currentCameraMesh = SimpleMesh::camera(currentCameraPose, 0.0015f);
+			SimpleMesh resultingMesh = SimpleMesh::joinMeshes(currentDepthMesh, currentCameraMesh, Matrix4f::Identity());
+
+			std::stringstream ss;
+			ss << filenameBaseOut << sensor.getCurrentFrameCnt() << ".off";
+			std::cout << filenameBaseOut << sensor.getCurrentFrameCnt() << ".off" << std::endl;
+			if (!resultingMesh.writeMesh(ss.str())) {
+				std::cout << "Failed to write mesh!\nCheck file path!" << std::endl;
+				return -1;
+			}
+		}
+		
+		i++;
+	}
+
+	delete optimizer;
+
+	return 0;
+}
+
+int main() {
+	int result = 0;
+	if (RUN_SEQUENCE_ICP)
+		result += reconstructRoom();
+
+	return result;
+}
